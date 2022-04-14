ab91d671589e9acddd2b8589dc4737ff101a98fa
feres1994 <feresfatnassi@gmail.com>
2022-04-14 17:35:04 +0100

    add dropdown  partial component

---
+++ dynamic_form/src/components/form/Form.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <DropDown :options="getMainData" @select-option="selectOption" />
+  </div>
+</template>
+
+<script>
+import DropDown from "../partials/DropDown.vue";
+import data from "./data";
+export default {
+  name: "Form",
+  components: {
+    DropDown,
+  },
+  computed: {
+    getMainData() {
+      return data.map((el) => el.id);
+    },
+   
+  },
+  methods:{
+     selectOption(value) {
+      console.log(value);
+    },
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
